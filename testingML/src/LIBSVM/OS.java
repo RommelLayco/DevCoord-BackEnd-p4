8bb7bcb991d2c7a8dc4461111bd16bb7b59644b1
Rommel <rj.layco@gmail.com>
2016-03-17 19:08:31 +1300

    added method to get os enum and test

--- testingML/src/LIBSVM/OS.java
+++ testingML/src/LIBSVM/OS.java
@@ -6,5 +6,29 @@ package LIBSVM;
  *
  */
 public enum OS {
-	ALL, WINDOWS, LINUX, MAC
+	ALL, WINDOWS_XP, WINDOWS_VISTA, WINDOWS_SERVER,
+	WINDOWS7, LINUX, MAC, MAC_COCOA;
+	
+	public static OS osEnum(String word){
+		if(word.equals("All")){
+			return ALL;
+		} else if(word.equals("Windows XP")){
+			return WINDOWS_XP;
+		} else if(word.equals("Windows Vista")){
+			return WINDOWS_VISTA;
+		} else if(word.equals("Windows Server 2003")){
+			return WINDOWS_SERVER;
+		} else if(word.equals("Windows 7")){
+			return WINDOWS7;
+		} else if(word.equals("Linux")){
+			return LINUX;
+		} else if(word.equals("Mac OS X")){
+			return MAC;
+		} else if(word.equals("Mac OS X - Cocoa")){
+			return MAC_COCOA;
+		} else{
+			return null;
+		}
+				
+	}
 }
--- testingML/src/LIBSVMTest/TestEnum.java
+++ testingML/src/LIBSVMTest/TestEnum.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import LIBSVM.OS;
 import LIBSVM.Platform;
 
 public class TestEnum {
@@ -16,6 +17,19 @@ public class TestEnum {
 		assertEquals(Platform.MACINTOSH, Platform.platformEnum("Macintosh"));
 		assertEquals(Platform.SUN, Platform.platformEnum("Sun"));
 		assertNull(Platform.platformEnum("sdvdhlfj"));
+	}
+	
+	@Test
+	public void testOS(){
+		assertEquals(OS.ALL, OS.osEnum("All"));
+		assertEquals(OS.WINDOWS_XP, OS.osEnum("Windows XP"));
+		assertEquals(OS.WINDOWS_VISTA, OS.osEnum("Windows Vista"));
+		assertEquals(OS.WINDOWS_SERVER, OS.osEnum("Windows Server 2003"));
+		assertEquals(OS.WINDOWS7, OS.osEnum("Windows 7"));
+		assertEquals(OS.LINUX, OS.osEnum("Linux"));
+		assertEquals(OS.MAC, OS.osEnum("Mac OS X"));
+		assertEquals(OS.MAC_COCOA, OS.osEnum("Mac OS X - Cocoa"));
+		assertNull(OS.osEnum("dslkfl"));
 	}
 
 }
