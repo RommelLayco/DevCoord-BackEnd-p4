197c5bc63e8df376689251566815ede6f3755bc2
Rommel <rj.layco@gmail.com>
2016-03-17 19:53:42 +1300

    created method to intialze task object and test

--- testingML/src/LIBSVM/Task.java
+++ testingML/src/LIBSVM/Task.java
@@ -11,12 +11,47 @@ public class Task {
 	private Platform platform;
 	private OS os;
 	private Component component;
-	
+
 	public Task(int taskID, Platform platform, OS os, Component component){
 		this.taskID = taskID;
 		this.platform = platform;
 		this.os = os;
 		this.component = component;
+	}
+
+	/**
+	 * 
+	 * @param line
+	 * @return
+	 */
+	public static Task create(String[] line){
+		//convert into right type
+		int taskID = Integer.parseInt(line[0]);
+		Platform platform = Platform.platformEnum(line[9]);
+		OS os = OS.osEnum(line[10]);
+		Component component = Component.componentEnum(line[12]);
+		
+		//Create task pair object
+		Task task = new Task(taskID, platform, os, component);
+		return task;
+		
+	}
+	
+
+	// ***************** getter methods for junit testing ***************************
+	public int getTaskID(){
+		return this.taskID;
+	}
+	public Platform getPlatform(){
+		return this.platform;
+	}
+
+	public OS getOS(){
+		return this.os;
+	}
+
+	public Component getComponent(){
+		return this.component;
 	}
 	
 }
---
+++ testingML/src/LIBSVMTest/TestTask.java
@@ -0,0 +1,29 @@
+package LIBSVMTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import LIBSVM.Component;
+import LIBSVM.OS;
+import LIBSVM.Platform;
+import LIBSVM.Task;
+
+public class TestTask {
+
+	@Test
+	public void testCreate() {
+		String[] line = {"164221","11/11/2006 12:11", "5/01/2008 17:17",
+				"3/03/2010 21:52", "Frank",	"RESOLVED",	"0",
+				"671.1909722", "P3", "PC", "Windows XP", "Mylyn", "Bugzilla", "enhancement"};
+	
+		Task task = Task.create(line);
+		
+		assertEquals(164221, task.getTaskID());
+		assertEquals(Platform.PC, task.getPlatform());
+		assertEquals(OS.WINDOWS_XP, task.getOS());
+		assertEquals(Component.BUGZILLA, task.getComponent());
+		
+	}
+
+}
