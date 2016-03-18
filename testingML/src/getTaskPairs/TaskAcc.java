b639284f225b3d9a23f41b92be77b4a2a1a2b26f
Rommel <rj.layco@gmail.com>
2016-03-18 14:59:50 +1300

    task acc will change critical to true now when needed

--- testingML/src/getTaskPairs/TaskAcc.java
+++ testingML/src/getTaskPairs/TaskAcc.java
@@ -18,7 +18,7 @@ public class TaskAcc {
 
 	public TaskAcc(int task1, int task2, Version version,
 			boolean discussion1, boolean discussion2, boolean conflict1, boolean conflict2){
-		
+
 		this.task1 = task1;
 		this.task2 = task2;
 		this.version = version;
@@ -28,16 +28,18 @@ public class TaskAcc {
 		this.conflict2 = conflict2;
 		this.critical = false;
 	}
-	
+
 	/**
 	 * Set critical field to true or false
 	 * by looking at the booleans 
 	 */
-	private void setCritical(){
-		
+	public void setCritical(){
+		if((this.discussion1 && this.discussion2) || (this.conflict1 && this.conflict2)){
+			this.critical = true;
+		}
 	}
-	
-	
+
+
 	/**
 	 * Helper method to create taskAcc object from a line in the file
 	 * @param line a string array of words
@@ -52,14 +54,15 @@ public class TaskAcc {
 		boolean d2 = readWord(line[7]);
 		boolean c1 = readWord(line[8]);
 		boolean c2 = readWord(line[9]);
-		
+
 		//Create task pair object
 		TaskAcc taskAcc = new TaskAcc(t1, t2, version, d1, d2, c1, c2);
-		
+		taskAcc.setCritical();
+
 		return taskAcc;
 	}
-	
-	
+
+
 	//******************************** HELPER METHOD TO CONVERT INPUT INTO BOOLEAN ************************
 	private static boolean readWord(String word){
 		if(word.equals("Not")){
@@ -68,38 +71,38 @@ public class TaskAcc {
 			return true;
 		}
 	}
-	
+
 	//********************* HELPER METHOD to test input *******************88
 	public int getT1(){
 		return this.task1;
 	}
-	
+
 	public int getT2(){
 		return this.task2;
 	}
-	
+
 	public Version getVersion(){
 		return this.version;
 	}
-	
+
 	public boolean isD1(){
 		return this.discussion1;
 	}
-	
+
 	public boolean isD2(){
 		return this.discussion2;
 	}
-	
+
 	public boolean isC1(){
 		return this.conflict1;
 	}
-	
+
 	public boolean isC2(){
 		return this.conflict2;
 	}
-	
+
 	public boolean isCritical(){
 		return this.critical;
 	}
-	
+
 }
--- testingML/src/testGetTaskPairs/TestTaskAcc.java
+++ testingML/src/testGetTaskPairs/TestTaskAcc.java
@@ -28,7 +28,30 @@ public class TestTaskAcc {
 		assertFalse(taskAcc.isD2());
 		assertFalse(taskAcc.isC1());
 		assertFalse(taskAcc.isC2());
-		assertFalse(taskAcc.isCritical());
+		assertFalse(taskAcc.isCritical());	
+	}
+	
+	/**
+	 * Test to see if it sets critical to correct value
+	 */
+	@Test
+	public void testSetCritical(){
+		
+		TaskAcc t1 = new TaskAcc(1,2,Version.TEST,true,true, true, false);
+		TaskAcc t2 = new TaskAcc(1,2,Version.TEST,true,false, false, false);
+		TaskAcc t3 = new TaskAcc(1,2,Version.TEST,true,false, true, true);
+		TaskAcc t4 = new TaskAcc(1,2,Version.TEST,false,false, false, true);
+		
+		t1.setCritical();
+		t2.setCritical();
+		t3.setCritical();
+		t4.setCritical();
+		
+		assertTrue(t1.isCritical());
+		assertFalse(t2.isCritical());
+		assertTrue(t3.isCritical());
+		assertFalse(t4.isCritical());
+		
 		
 	}
 
