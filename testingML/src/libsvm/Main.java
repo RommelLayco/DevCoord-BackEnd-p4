73859071fedd473b2c075f310833f572f7abd1ef
Rommel <rj.layco@gmail.com>
2016-03-18 19:27:24 +1300

    created a folder to store generated files from code, contents in file will be ignored

---
+++ testingML/src/libsvm/Main.java
@@ -0,0 +1,41 @@
+package libsvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import getTaskPairs.ProcessData;
+import getTaskPairs.ReadFiles;
+import getTaskPairs.Task;
+import getTaskPairs.TaskAcc;
+import getTaskPairs.TaskPair;
+import getTaskPairs.TaskPairKey;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//List of taskPair keys
+		List<TaskPairKey> keys = new ArrayList<TaskPairKey>();
+		
+		//List of taskAcc keys
+		List<TaskPairKey> accKeys = new ArrayList<TaskPairKey>();
+		
+		List<String[]> lines = ReadFiles.readFile("input", "tasks_3_2.csv");
+		Map<Integer, Task> tasks = ReadFiles.makeTaskMap(lines);
+		
+		List<String[]> lines2 = ReadFiles.readFile("input", "pairs_3_2.csv");
+		Map<TaskPairKey, TaskPair> taskPairs = ReadFiles.makeTaskPairMap(lines2, keys);
+		
+		List<String[]> lines3 = ReadFiles.readFile("input", "accuracy_coding.csv");
+		Map<TaskPairKey, TaskAcc> taskAcc = ReadFiles.makeTaskAccMap(lines3, accKeys);
+		
+		//process data
+		ProcessData data = new ProcessData(taskPairs, tasks, keys, taskAcc, accKeys);
+		data.setMatching();
+		data.setCritical();
+		
+		//write data to files
+
+	}
+
+}
---
+++ testingML/src/libsvm/WritetoFile.java
@@ -0,0 +1,5 @@
+package libsvm;
+
+public class WritetoFile {
+	
+}
