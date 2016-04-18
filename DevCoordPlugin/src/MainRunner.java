ebbfc03e0ec4cf5496f778e65a9d97694c22d134
jchi098 <jchi098@aucklanduni.ac.nz>
2016-04-18 18:20:40 +1200

    working with repo and bugzilla

--- DevCoordPlugin/src/MainRunner.java
+++ DevCoordPlugin/src/MainRunner.java
@@ -3,17 +3,43 @@ import devcoordplugin.handlers.SampleHandler;
 
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.mylyn.*;
 import org.eclipse.mylyn.internal.monitor.ui.MonitorUiPlugin;
+import org.eclipse.mylyn.internal.tasks.core.AbstractTask;
 import org.eclipse.mylyn.internal.tasks.core.TaskList;
 import org.eclipse.mylyn.internal.tasks.core.sync.GetTaskHistoryJob;
-
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+import org.eclipse.mylyn.tasks.core.data.TaskData;
+import org.eclipse.mylyn.internal.bugzilla.*;
+import org.eclipse.mylyn.internal.bugzilla.core.BugzillaCorePlugin;
+import org.eclipse.mylyn.internal.bugzilla.core.BugzillaRepositoryConnector;
 public class MainRunner {
 
 	public static void main(String[] args) {
 		
+		String url="https://bugs.eclipse.org/bugs";
+			TaskRepository repository = new TaskRepository(BugzillaCorePlugin.CONNECTOR_KIND, url);
+			
+			System.out.println(repository.toString());
+	
+			//System.out.println(repository.get);
+			
+			BugzillaRepositoryConnector connector = new BugzillaRepositoryConnector();
+	
+			try {
+				TaskData taskData = connector.getTaskData(repository, "1", null);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+TaskList t=new TaskList();
+t.getAllTasks();
 
-		System.out.println(MonitorUiPlugin.getDefault().toString());
+for (AbstractTask task : t.getAllTasks()) {
+	System.out.println(task.toString());
+}
 		
 //System.out.println(new MonitorUi().);
 	
