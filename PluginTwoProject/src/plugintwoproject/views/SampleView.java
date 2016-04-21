25dd511345b24d7f7782c7e1f72f7fc2c38bb893
jchi098 <jchi098@aucklanduni.ac.nz>
2016-04-22 01:38:37 +1200

    Prints all the task info into the Plugin, refreshes when ther is activity in mylyn

--- PluginTwoProject/src/plugintwoproject/views/SampleView.java
+++ PluginTwoProject/src/plugintwoproject/views/SampleView.java
@@ -1,19 +1,23 @@
 package plugintwoproject.views;
 
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-
-import java.util.Collection;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.SWT;
-import org.eclipse.mylyn.internal.tasks.core.AbstractTask;
-import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.*;
+
+import helper.TaskInfo;
+
+import org.eclipse.jface.viewers.*;
+import org.eclipse.mylyn.commons.notifications.ui.AbstractUiNotification;
+import org.eclipse.mylyn.internal.tasks.ui.ITaskListNotificationProvider;
+import java.util.Set;
+
+import org.eclipse.jface.action.*;
+
+import org.eclipse.ui.*;
+
+
 
 
 /**
@@ -35,48 +39,21 @@ import org.eclipse.mylyn.tasks.core.ITask;
  */
 
 @SuppressWarnings("restriction")
-public class SampleView extends ViewPart {
+public class SampleView extends ViewPart implements ITaskListNotificationProvider  {
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "projtwo.views.SampleView";
 
-	private TableViewer viewer;
-	private Action action1;
-	private Action action2;
-	private Action doubleClickAction;
-	//private final InteractionEventLogger logger;
-	/*
-	 * The content provider class is responsible for
-	 * providing objects to the view. It can wrap
-	 * existing objects in adapters or simply return
-	 * objects as-is. These objects may be sensitive
-	 * to the current input of the view, or ignore
-	 * it and always show the same content 
-	 * (like Task List, for example).
-	 */
 
-	class ViewContentProvider implements IStructuredContentProvider {
-		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
-		}
-		public void dispose() {
-		}
-		public Object[] getElements(Object parent) {
-			return new String[] { "One", "Two", "Three" };
-		}
-	}
-	class ViewLabelProvider extends LabelProvider implements ITableLabelProvider {
-		public String getColumnText(Object obj, int index) {
-			return getText(obj);
-		}
-		public Image getColumnImage(Object obj, int index) {
-			return getImage(obj);
-		}
-		public Image getImage(Object obj) {
-			return PlatformUI.getWorkbench().
-					getSharedImages().getImage(ISharedImages.IMG_OBJ_ELEMENT);
-		}
-	}
+	
+private Text text;
+	
+	private Action action1;
+
+
+
+
 	class NameSorter extends ViewerSorter {
 	}
 
@@ -84,7 +61,10 @@ public class SampleView extends ViewPart {
 	 * The constructor.
 	 */
 	public SampleView() {
-		//	logger = UiUsageMonitorPlugin.getDefault().getInteractionLogger();
+		super();
+		org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin.getTaskListNotificationManager().addNotificationProvider(this);;
+
+
 
 
 	}
@@ -94,32 +74,33 @@ public class SampleView extends ViewPart {
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		viewer.setContentProvider(new ViewContentProvider());
-		viewer.setLabelProvider(new ViewLabelProvider());
-		viewer.setSorter(new NameSorter());
-		viewer.setInput(getViewSite());
 
-		// Create the help context id for the viewer's control
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "projTwo.viewer");
-		getSite().setSelectionProvider(viewer);
+	    text = new Text(parent, SWT.MULTI | SWT.BORDER | SWT.WRAP | SWT.V_SCROLL);
+	   
+
+		
+
+		RefreshDevCoord();
+
+
+	
+		
 		makeActions();
-		hookContextMenu();
-		hookDoubleClickAction();
+	
+
 		contributeToActionBars();
 	}
 
-	private void hookContextMenu() {
-		MenuManager menuMgr = new MenuManager("#PopupMenu");
-		menuMgr.setRemoveAllWhenShown(true);
-		menuMgr.addMenuListener(new IMenuListener() {
-			public void menuAboutToShow(IMenuManager manager) {
-				SampleView.this.fillContextMenu(manager);
-			}
-		});
-		Menu menu = menuMgr.createContextMenu(viewer.getControl());
-		viewer.getControl().setMenu(menu);
-		getSite().registerContextMenu(menuMgr, viewer);
+	
+	private void RefreshDevCoord(){
+
+
+		
+		text.setText(TaskInfo.getTasksInfoAsAString());
+
+
+		
+		
 	}
 
 	private void contributeToActionBars() {
@@ -131,137 +112,52 @@ public class SampleView extends ViewPart {
 	private void fillLocalPullDown(IMenuManager manager) {
 		manager.add(action1);
 		manager.add(new Separator());
-		manager.add(action2);
+
 	}
 
-	private void fillContextMenu(IMenuManager manager) {
-		manager.add(action1);
-		manager.add(action2);
-		// Other plug-ins can contribute there actions here
-		manager.add(new Separator(IWorkbenchActionConstants.MB_ADDITIONS));
-	}
+
+
+
 
 	private void fillLocalToolBar(IToolBarManager manager) {
 		manager.add(action1);
-		manager.add(action2);
+
 	}
 
 	private void makeActions() {
 		action1 = new Action() {
 			public void run() {
-				showMessage("Action 1 executed");
+				RefreshDevCoord();
+
 			}
 		};
-		action1.setText("Action 1");
-		action1.setToolTipText("Action 1 tooltip");
+		action1.setText("Refresh DevCoord");
+		action1.setToolTipText("Press to Referesh DevCoord");
 		action1.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-
-		action2 = new Action() {
-			public void run() {
-				showMessage("Action 2 executed");
-			}
-		};
-		action2.setText("Action 2");
-		action2.setToolTipText("Action 2 tooltip");
-		action2.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(ISharedImages.IMG_OBJS_INFO_TSK));
-		
-		
-		doubleClickAction = new Action() {
-			public void run() {
-				
-				org.eclipse.mylyn.internal.tasks.core.TaskList list=org.eclipse.mylyn.internal.tasks.ui.TasksUiPlugin.getTaskList();
+				getImageDescriptor(ISharedImages.IMG_ELCL_SYNCED));
 
 
-
-
-				Collection<AbstractTask> tasks=list.getAllTasks();
-
-				for (AbstractTask task : tasks) {
-					System.out.println("=====================================");
-					System.out.println("TASK:"+task.toString());
-					System.out.println("	TASK ID:"+task.getTaskId());
-					System.out.println("	TASK Owner:"+task.getOwner()+". ID:"+task.getOwnerId());
-					System.out.println("	TASK Priority:"+task.getPriority());
-					System.out.println("	TASK URL:"+task.getUrl());
-
-					System.out.println("	TASK Repository URL:"+task.getRepositoryUrl());
-
-					System.out.println("	TASK KIND:"+task.getTaskKind());
-					System.out.println("	TASK Child Tasks-");
-					for (ITask child : task.getChildren()) {
-
-						System.out.println("		Child  TASK :"+child.toString());
-					}
-
-					System.out.println("	TASK Status:"+task.getStatus());
-
-					System.out.println("=====================================");
-
-
-
-
-
-
-
-
-
-				}
-
-			}
-		};
 	}
 
-	private void hookDoubleClickAction() {
-		viewer.addDoubleClickListener(new IDoubleClickListener() {
-			public void doubleClick(DoubleClickEvent event) {
-				doubleClickAction.run();
-			}
-		});
-	}
-	private void showMessage(String message) {
-		MessageDialog.openInformation(
-				viewer.getControl().getShell(),
-				"Sample View",
-				message);
-	}
+
 
 	/**
 	 * Passing the focus request to the viewer's control.
 	 */
 	public void setFocus() {
-		viewer.getControl().setFocus();
+		text.setFocus();
+//		viewer.getControl()   .setFocus();
 	}
 
-	//	@Override
-	//	public void partActivated(IWorkbenchPart part) {
-	//		System.out.println("PART ACTIVATED");
-	//		
-	//	}
-	//
-	//	@Override
-	//	public void partBroughtToTop(IWorkbenchPart part) {
-	//		System.out.println("PART partBroughtToTop");
-	//		
-	//	}
-	//
-	//	@Override
-	//	public void partClosed(IWorkbenchPart part) {
-	//		System.out.println("PART partClosed");
-	//		
-	//		
-	//	}
-	//
-	//	@Override
-	//	public void partDeactivated(IWorkbenchPart part) {
-	//		System.out.println("PART partDeactivated");
-	//		
-	//	}
-	//
-	//	@Override
-	//	public void partOpened(IWorkbenchPart part) {
-	//		System.out.println("PART partOpened");
-	//		
-	//	}
+	@Override
+	public Set<AbstractUiNotification> getNotifications() {
+		System.out.println("NOTIDIED");
+		text.setText(TaskInfo.getTasksInfoAsAString());
+			return null;
+	}
+
+	
+
+
+
 }
