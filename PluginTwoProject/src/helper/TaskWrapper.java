9f436090606df8df7aa00bcc49bab183a765655e
jchi098 <jchi098@aucklanduni.ac.nz>
2016-04-28 20:22:05 +1200

    Added getters and toString

--- PluginTwoProject/src/helper/TaskWrapper.java
+++ PluginTwoProject/src/helper/TaskWrapper.java
@@ -21,7 +21,23 @@ import org.eclipse.mylyn.tasks.core.ITask;
  * 
  * 
  * */
+
+@SuppressWarnings("restriction")
 public class TaskWrapper {
+
+
+	/**
+	 * By calling this static method,a TaskWrapper object is created with the desired
+	 * informaton.
+	 * 
+	 * This method should only be called by the triggering of the InteractionEvent listener.
+	 * 
+	 * */
+	public static TaskWrapper getTaskWrapper(InteractionEvent eventArg){
+		
+		return  new TaskWrapper(eventArg);
+		
+	}
 
 	private String taskID;
 	private String taskHandle;
@@ -41,7 +57,7 @@ public class TaskWrapper {
 		
 		taskID=activeTask.getTaskId();
 		taskHandle=activeTask.getHandleIdentifier();
-		
+		taskLabel="DUMMY_LABEL";
 		
 		
 		
@@ -62,12 +78,54 @@ public class TaskWrapper {
 	
 	
 	
-	
-	
-	
-	public void getInteractionKind() {
-		// TODO Auto-generated method stub
+
+	public String getTaskID() {
+		return taskID;
+	}
+
+
+
+	public String getTaskHandle() {
+		return taskHandle;
+	}
+
+
+
+	public String getTaskLabel() {
+		return taskLabel;
+	}
+
+
+
+	public Kind getInteractionEventKind() {
+		return interactionEventKind;
+	}
+
+
+
+	public String getStructureHandle() {
+		return structureHandle;
+	}
+
+	@Override
+	public String toString() {	
+
+		String separator=System.getProperty("line.separator");
+		
+		String toReturn="";
+		
+		
+		toReturn+="TaskID:"+getTaskID()+separator;
+		toReturn+="TaskLabel:"+getTaskLabel()+separator;
+		toReturn+="TaskHandle:"+getTaskHandle()+separator;
+		toReturn+="EventKind:"+getInteractionEventKind()+separator;
+		toReturn+="StructureHandle:"+getStructureHandle()+separator;
+		
+		
+
+		return toReturn;
 
 	}
+	
 
 }
