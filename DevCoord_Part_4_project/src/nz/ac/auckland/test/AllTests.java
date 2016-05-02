497c3b58c8dd3cf4642f709da80bfcd68b6878e2
Rommel <rj.layco@gmail.com>
2016-05-02 18:17:59 +1200

    created test suite

---
+++ DevCoord_Part_4_project/src/nz/ac/auckland/test/AllTests.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Database.class, ReadXMLTest.class, TaskPairTest.class })
+public class AllTests {
+
+}
