5c2f713ba4b3ba1c9110bdad6ce7ee625cf7e662
Rommel <rj.layco@gmail.com>
2016-05-21 19:10:37 +1200

    saving work before changing branch

---
+++ DevCoord_Part_4_project/src/controller/DatabaseMethods.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class DatabaseMethods {
+	
+	private SessionFactory sessionFactory;
+	private Session session;
+	
+
+}
