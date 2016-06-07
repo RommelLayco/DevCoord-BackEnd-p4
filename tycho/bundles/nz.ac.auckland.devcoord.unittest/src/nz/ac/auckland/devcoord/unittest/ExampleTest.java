079bc8b56e1f3365722c8a5733c67e276534bcd2
Rommel <rj.layco@gmail.com>
2016-06-07 13:11:48 +1200

    created unit test with chishti

---
+++ tycho/bundles/nz.ac.auckland.devcoord.unittest/src/nz/ac/auckland/devcoord/unittest/ExampleTest.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.devcoord.unittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+        @Test
+        public void test() {
+                // just an example
+                assertTrue(true);
+        }
+
+}
