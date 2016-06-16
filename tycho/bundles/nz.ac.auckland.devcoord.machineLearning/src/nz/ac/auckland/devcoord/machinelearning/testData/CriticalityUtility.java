9b8eabc7aaf4aa716ceb3838561c1d90ab54b6ea
jchi098 <jchi098@aucklanduni.ac.nz>
2016-06-17 00:46:02 +1200

    Moved methods dealing with test Taskpaircriticality to this utliity class

--- tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/testData/CriticalityUtility.java
+++ tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/testData/CriticalityUtility.java
@@ -1,8 +1,10 @@
-package nz.ac.auckland.devcoord.machinelearning;
+package nz.ac.auckland.devcoord.machinelearning.testData;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
@@ -12,6 +14,9 @@ import java.util.List;
 import nz.ac.auckland.devcoord.database.Task;
 import nz.ac.auckland.devcoord.database.TaskPair;
 import nz.ac.auckland.devcoord.machinelearning.decisionTree.InputEnum;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
 /**
  * Utility class which will be used to fill in the criticality of the task pairs
  * */
@@ -78,6 +83,59 @@ public class CriticalityUtility {
 
 
 	}
+	
+	
+	/**
+	 * Return true if the value of isCritical already present is correct.
+	 * 
+	 * */
+	public static boolean wasClassificationCorrect()
+	{
+		
+		String trainString;
+		String testString;
+	
+			trainString=InputEnum.toString(InputEnum.PAIRS_3_2_Train_Output_NODRH);
+			testString=InputEnum.toString(InputEnum.PAIRS_3_2_Test_Output_NODRH);
+	
+			// train classifier
+		     J48 cls = new J48();
+		     Instances train;
+		     Instances test;
+		     
+			try {
+				train = new Instances(new BufferedReader(new FileReader(trainString)));
+				train.setClassIndex(train.numAttributes() - 1);
+				
+				
+				test = new Instances(new BufferedReader(new FileReader(testString)));
+				test.setClassIndex(test.numAttributes() - 1);
+				
+
+
+			
+				cls.setUnpruned(false);
+				
+				
+			     cls.buildClassifier(train);
+			     
+			     Evaluation eval = new Evaluation(train);
+			     eval.evaluateModel(cls, test);
+			     
+			    
+			     return eval.correct()>0;
+			
+			}
+			
+			catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return false;
+		
+	}
+	
+	
 
 	/**
 	 * Helper method- creates a label which is written in the arff file,depending on the arguments,
