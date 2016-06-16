9c9b23c55a332d6c237b3d31dfb5706f51e2a038
jchi098 <jchi098@aucklanduni.ac.nz>
2016-06-17 01:14:08 +1200

    cleaned up train data generation file

--- tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/decisionTree/TrainDataGeneration.java
+++ tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/decisionTree/TrainDataGeneration.java
@@ -14,7 +14,7 @@ import nz.ac.auckland.devcoord.machinelearning.trainData.TaskPairKey;
 /**
  * To run the algorithm
  * */
-public class MainClass {
+public class TrainDataGeneration {
 
 	public static void main(String[] args) {
 
@@ -52,39 +52,12 @@ public class MainClass {
 //		makeARFFAndTree(data,true);
 
 		/**PRUNED*/
-		makeARFFAndTree(data,false);
-
-
-
-
-	}
-	/**
-	 * Calls convert method to make arff files from csvs,
-	 * and Make method to create a tree and make a report for the test set classification.
-	 * */
-	private static void makeARFFAndTree(ProcessData data,boolean unpruned){
-
-
 		DataToARFF.convertTrainData(data, InputEnum.PAIRS_3_2);
-		
-		
-	//	String[] lines={"0","1","FALSE","TRUE","FALSE","0.35454","-1","-1"};
-
-		
-		CriticalityUtility.testDummyTestData(); 
-
-		System.out.println("MakeTree.wasClassificationCorrect():"+CriticalityUtility.wasClassificationCorrect());
-		System.out.println("Done");
-		
-		
-		
-		
-		
-		//MakeTree.make(false,false);
 
 
 
 
 	}
+
 
 }
