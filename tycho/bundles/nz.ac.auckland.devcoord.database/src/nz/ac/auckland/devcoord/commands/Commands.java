963452d2088825ac21d2c9dc1e6a3ef7cc59b9ca
Rommel <rj.layco@gmail.com>
2016-06-20 19:05:22 +1200

    method to query task with the same context structures

--- tycho/bundles/nz.ac.auckland.devcoord.database/src/nz/ac/auckland/devcoord/commands/Commands.java
+++ tycho/bundles/nz.ac.auckland.devcoord.database/src/nz/ac/auckland/devcoord/commands/Commands.java
@@ -1,6 +1,12 @@
 package nz.ac.auckland.devcoord.commands;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import nz.ac.auckland.devcoord.database.Task;
 
@@ -55,4 +61,69 @@ public class Commands {
 		em.close();
 	}
 
+	/**
+	 * Method gets tasks from database that contains the
+	 * same context structures the task has.
+	 * 
+	 * @param task with the context structures. Look for other task
+	 * with same context structures.
+	 */
+	public List<Task> getTaskWithSameContext(Task task){
+		List<Task> tasks = new ArrayList<Task>(); 
+		
+		//create query string
+		String jpql = "Select t from Task t, IN(t.contextStructure) c"
+				+ " where t.taskID != " + task.getTaskID();
+		
+		
+		Set<String> keys = task.getContextStructures().keySet();
+		if(keys.size() > 0){
+			String line = contextStructureJPQL(keys);
+			
+			jpql = jpql + " AND (" + line + ")";
+			
+//			jpql = "Select t from Task t, IN(t.contextStructure) c"
+//					+ " where c.name = 'queryTest'";
+			
+			EntityManager em = hibernateUtil.getEntityManager();
+			Query query = em.createQuery(jpql);
+			
+			tasks = query.getResultList();
+			em.close();
+			
+			return tasks;
+		} else{
+			return tasks;
+		}
+		
+		
+		
+	}
+	
+	/**
+	 * Formats the the "or's" of the context structures
+	 * @param name
+	 * @return
+	 */
+	private static String contextStructureJPQL(Set<String> contextStructureNames){
+		String line = "";
+		
+		Iterator<String> it = contextStructureNames.iterator();
+		//put down the first context structure name
+		
+		//note name need to be put between '' marks
+		//else it thinks it is a column name
+		if(it.hasNext()){
+			String name  = it.next();
+			line = "c.name = '" + name +"'";
+		}
+		
+		//put down the rest
+		while(it.hasNext()){
+			String name = it.next();
+			line = line + " OR c.name = '" + name + "'";
+		}
+		
+		return line;
+	}
 }
