9152e9dd7c35912540da6fb18e77ed1d2bf05507
jchi098 <jchi098@aucklanduni.ac.nz>
2016-07-27 17:52:53 +1200

    Testing code that requires no new test file to be created everytime classification is needed

--- tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/MLImprovementSandBox/TimeClass.java
+++ tycho/bundles/nz.ac.auckland.devcoord.machineLearning/src/nz/ac/auckland/devcoord/machinelearning/MLImprovementSandBox/TimeClass.java
@@ -1,12 +1,19 @@
 package nz.ac.auckland.devcoord.machinelearning.MLImprovementSandBox;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
 import nz.ac.auckland.devcoord.database.Context_Structure;
@@ -14,8 +21,16 @@ import nz.ac.auckland.devcoord.database.Task;
 import nz.ac.auckland.devcoord.database.TaskPair;
 import nz.ac.auckland.devcoord.machinelearning.decisionTree.InputEnum;
 import nz.ac.auckland.devcoord.machinelearning.testData.CriticalityUtility;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 public class TimeClass {
+	static String outputTestDummy="output/DUMMY_test.arff";
+	
 /**
  * Timing code Reference:http://stackoverflow.com/questions/180158/how-do-i-time-a-methods-execution-in-java
  *
@@ -23,26 +38,119 @@ public class TimeClass {
  * */
 	public static void main(String[] args) {
 
-		TaskPair taskPair=buildDummyTaskPairOne();
+//		TaskPair taskPair=buildDummyTaskPairOne();
+//		
+//		
+//		long startTime = System.nanoTime();
+//
+//
+//		//CriticalityUtility.convertTestDataToArff(taskPair);
+//		CriticalityUtility.wasClassificationCorrect();
+//		
+//		
+//		long endTime = System.nanoTime();
+//
+//		long duration = (endTime - startTime)/1000000;  // milliseconds.
+//		
+//		System.out.println(duration);
+//		
+//		
+//		buildARFFTestFileWithMultipleTaskPairs();
 		
 		
-		long startTime = System.nanoTime();
-
-
-		//CriticalityUtility.convertTestDataToArff(taskPair);
-		CriticalityUtility.wasClassificationCorrect();
+		System.out.println(classifyWithoutFile());
 		
-		
-		long endTime = System.nanoTime();
-
-		long duration = (endTime - startTime)/1000000;  // milliseconds.
-		
-		System.out.println(duration);
-		
-		
-		buildARFFTestFileWithMultipleTaskPairs();
 
 	}
+	
+	public static boolean classifyWithoutFile()
+	{
+		String trainString;
+		
+			trainString=InputEnum.toString(InputEnum.TRAIN_OUTPUT_PATH);
+			
+			// train classifier
+		     J48 cls = new J48();
+		     Instances train;
+		     Instances test;
+			try {
+				FileReader trainFileReader=new FileReader(trainString);
+				BufferedReader trainBufferedReader=new BufferedReader(trainFileReader);
+				train = new Instances(trainBufferedReader);
+				train.setClassIndex(train.numAttributes() - 1);
+			
+				//System.getProperty("file.separator")
+				String data="@relation pairsTask"+"\n"+
+						"@attribute Proximity numeric"+"\n"+
+						"@attribute Critical {true,false}"+"\n"+
+						"@data"+"\n"+
+						"4.0,true";
+				InputStream inputStream=new ByteArrayInputStream(data.getBytes());
+				
+
+			BufferedReader testBufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+				test = new Instances(testBufferedReader);				
+				test.setClassIndex(test.numAttributes() - 1);
+				cls.setUnpruned(false);
+			     cls.buildClassifier(train);
+			     Evaluation eval = new Evaluation(train);
+			     eval.evaluateModel(cls, test);
+			    trainBufferedReader.close();
+			    testBufferedReader.close();
+			    System.out.println(cls);
+			    System.out.println(eval.correct());
+			    
+			    return eval.correct()>0;
+			}
+			catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return false;
+	}
+	
+	
+	
+	public static boolean classify()
+	{
+		String trainString;
+		String testString;
+			trainString=InputEnum.toString(InputEnum.TRAIN_OUTPUT_PATH);
+			testString=outputTestDummy;
+			// train classifier
+		     J48 cls = new J48();
+		     Instances train;
+		     Instances test;
+			try {
+				FileReader trainFileReader=new FileReader(trainString);
+				BufferedReader trainBufferedReader=new BufferedReader(trainFileReader);
+				train = new Instances(trainBufferedReader);
+				train.setClassIndex(train.numAttributes() - 1);
+				FileReader testFileReader=new FileReader(testString);
+				BufferedReader testBufferedReader=new BufferedReader(testFileReader);
+				test = new Instances(testBufferedReader);
+				
+				test.setClassIndex(test.numAttributes() - 1);
+				cls.setUnpruned(false);
+			     cls.buildClassifier(train);
+			     Evaluation eval = new Evaluation(train);
+			     eval.evaluateModel(cls, test);
+			    trainBufferedReader.close();
+			    testBufferedReader.close();
+			    System.out.println(cls);
+			    System.out.println(eval.correct());
+			    
+			    return eval.correct()>0;
+			}
+			catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			return false;
+	}
+	
+
+	
 	/**
 	 * 
 	 * Builds ARFF file with multiple task pairs*/
@@ -51,16 +159,16 @@ public class TimeClass {
 		
 		Writer writer = null;
 		String inputString;
-		inputString="output/DUMMY_test.arff";
+		inputString=outputTestDummy;
 		try{
 			FileOutputStream fileOutputStream=new FileOutputStream(inputString);
 			OutputStreamWriter outputStreamWriter=new OutputStreamWriter(fileOutputStream, "utf-8"); 
 			writer = new BufferedWriter(outputStreamWriter);
 			writer.write(CriticalityUtility.getLabels());
 			writer.write("@data"+"\n");
-			writer.write(CriticalityUtility.getStringOfTheTestTaskPair(buildDummyTaskPairOne()));
-			writer.write("\n");
 			writer.write(CriticalityUtility.getStringOfTheTestTaskPair(buildDummyTaskPairTwo()));
+			writer.write("\n");
+			writer.write(CriticalityUtility.getStringOfTheTestTaskPair(buildDummyTaskPairOne()));
 			writer.write("\n");
 			
 			writer.close();
