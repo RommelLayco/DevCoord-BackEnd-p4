c1cb3a3db951447999d29106354e5cff8cba7b9f
Rommel <rj.layco@gmail.com>
2016-07-28 21:45:19 +1200

    removed unused imports

--- tycho/bundles/nz.ac.auckland.devcoord.database/src/nz/ac/auckland/devcoord/database/Task.java
+++ tycho/bundles/nz.ac.auckland.devcoord.database/src/nz/ac/auckland/devcoord/database/Task.java
@@ -8,7 +8,6 @@ import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
